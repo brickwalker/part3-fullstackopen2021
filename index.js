5a57c283586599cb19961f0228eb00a5744f9a52
brickwalker <artem.nedostup@gmail.com>
2021-12-06 21:00:12 +0000

    3.3: Phonebook backend step3

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const express = require("express");
 const data = require("./data.json");
 
@@ -8,8 +9,14 @@ app.get("/api/persons", (req, res) => {
   res.json(data);
 });
 
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = data.find((item) => item.id === id);
+  person ? res.json(person) : res.status(404).end();
+});
+
 app.get("/info", (req, res) => {
-const len = data ? data.length : 0;
+  const len = data ? data.length : 0;
   res.send(`<div>
                 <p>Phonebook has info for ${len} people</p>
                 <p>${new Date()}</p>
