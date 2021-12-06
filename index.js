9fc2871b2eb8d68265986f962d4bab34424d3ead
brickwalker <artem.nedostup@gmail.com>
2021-12-06 20:21:06 +0000

    3.2: Phonebook backend step2

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ app.get("/api/persons", (req, res) => {
   res.json(data);
 });
 
+app.get("/info", (req, res) => {
+const len = data ? data.length : 0;
+  res.send(`<div>
+                <p>Phonebook has info for ${len} people</p>
+                <p>${new Date()}</p>
+            </div>`);
+});
+
 app.listen(port, () => {
   console.log(`Phonebook api listening at http://localhost:${port}`);
 });
